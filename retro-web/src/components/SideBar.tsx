da445f61fa5fb7f0855eb7d6bdfafc690dc4e226
faraz ahmad <farazamiruddin@gmail.com>
2019-08-22 18:19:49 -0400

    add mailto link to sidebar

--- retro-web/src/components/SideBar.tsx
+++ retro-web/src/components/SideBar.tsx
@@ -1,13 +1,13 @@
 import * as React from "react";
 import { Nav, NavItem } from "reactstrap";
 import { Link } from "react-router-dom";
-import Octicon, { Home } from "@primer/octicons-react";
+import Octicon, { Home, RequestChanges } from "@primer/octicons-react";
 
 export class Sidebar extends React.Component {
   render() {
     return (
       <Nav vertical>
-        <NavItem>
+        <NavItem className="mb-3">
           <Link
             to="/dashboard"
             className="d-flex align-items-center text-muted"
@@ -15,6 +15,12 @@ export class Sidebar extends React.Component {
             <Octicon size="small" icon={Home} />
             <span className="ml-2">Home</span>
           </Link>
+        </NavItem>
+        <NavItem className="mb-3">
+          <a className="text-muted" href="mailto:faraz@retro.app">
+            <Octicon size="small" icon={RequestChanges} />
+            <span className="ml-2">Contact Us</span>
+          </a>
         </NavItem>
       </Nav>
     );
