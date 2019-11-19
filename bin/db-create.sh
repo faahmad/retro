0ff123b6b82cd9b511cad40f8b731183a347e0b2
faraz ahmad <farazamiruddin@gmail.com>
2019-11-18 22:28:47 -0500

    Scaffold new web app

--- bin/db-create.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Exit on error
-set -e
-
-psql -U me -d postgres -c "CREATE DATABASE retro_dev;"
-psql -U me -d postgres -c "CREATE DATABASE retro_test;"
--- bin/db-seed.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Exit on error
-set -e
-
-psql -U me -d retro_dev -f database/seed.sql
