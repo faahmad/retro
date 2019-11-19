abc54dffc5eff5649e2e12463f676453ebee8525
faraz ahmad <farazamiruddin@gmail.com>
2019-11-19 07:44:15 -0500

    Setup tailwindcss

--- web/src/components/App.tsx
+++ web/src/components/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className="text-xl">
       <h1>Retro</h1>
     </div>
   );
--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,6 +1,6 @@
+import "./styles/index.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
