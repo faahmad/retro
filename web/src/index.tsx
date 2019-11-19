195364c52de2a5f9931ce9557f9b6b23f221e624
faraz ahmad <farazamiruddin@gmail.com>
2019-11-19 08:42:57 -0500

    Setup AppRoutes component

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,10 +1,10 @@
 import "./styles/index.css";
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
+import { AppRoutes } from "./AppRoutes";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<AppRoutes />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ web/src/pages/LandingPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const LandingPage: React.FC = () => {
+  return (
+    <div className="container">
+      <h1>The Retrospective Tool For People Who Hate Retros.</h1>
+    </div>
+  );
+};
