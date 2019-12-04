e313fbba056e3b823da1c29e77b3244d3511edab
faraz ahmad <farazamiruddin@gmail.com>
2019-12-04 09:16:40 -0500

    Setup gcloud cloudsql config

--- server/src/lib/sequelize.js
+++ server/src/lib/sequelize.js
@@ -1,10 +1,22 @@
 import Sequelize from "sequelize";
 
+const isProd = process.env.NODE_ENV === "production";
+
+let config = {
+  dialect: "postgres"
+};
+
+if (isProd) {
+  const instancePath = `/cloudsql/${process.env.CLOUD_SQL_CONNECTION_NAME}`;
+  config.host = instancePath;
+  config.dialectOptions = {
+    socketPath: instancePath
+  };
+}
+
 export const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DATABASE_USER,
-  null,
-  {
-    dialect: "postgres"
-  }
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  isProd ? process.env.DB_PASSWORD : null,
+  config
 );
