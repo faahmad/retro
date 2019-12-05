c5169d52b33c79bf98df0fc3bc4f81255e14c581
Faraz Ahmad <faraz.ahmad@uber.com>
2019-12-05 12:56:55 -0500

    Setup config to use GC credentials

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,9 @@ import { sequelize } from "./lib/sequelize";
 
 (async function(sequelize, app) {
   await sequelize.sync();
+
+  app.get("/", (request, response) => response.send("sup."));
+
   app.listen({ port: 8000 }, () => {
     console.log("Apollo Server on http://localhost:8000/graphql");
   });
--- server/src/lib/sequelize.js
+++ server/src/lib/sequelize.js
@@ -1,9 +1,22 @@
 import Sequelize from "sequelize";
 
+let config = {
+  dialect: "postgres"
+};
+
+if (
+  process.env.INSTANCE_CONNECTION_NAME &&
+  process.env.NODE_ENV === "production"
+) {
+  const instancePath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+  config.host = instancePath;
+  config.dialectOptions = { socketPath: instancePath };
+}
+
 export const sequelize = new Sequelize(
-  process.env.DATABASE,
-  process.env.DATABASE_USER,
-  null,
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
   {
     dialect: "postgres"
   }
