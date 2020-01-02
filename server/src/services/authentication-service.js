447774258216540c8bdc1c58f0d0f2724f0b264a
faraz ahmad <farazamiruddin@gmail.com>
2020-01-02 01:58:25 -0500

    Update favicon

--- server/src/services/authentication-service.js
+++ server/src/services/authentication-service.js
@@ -1,13 +1,10 @@
-import admin from "firebase-admin";
-import serviceAccountKey from "../config/serviceAccountKey.dev.json";
+import firebaseAdmin from "../lib/firebase-admin";
 import { AuthenticationError } from "apollo-server-express";
-
-admin.initializeApp({ credential: admin.credential.cert(serviceAccountKey) });
 
 export class AuthenticationService {
   static async getUserIdFromIdToken(idToken) {
     try {
-      const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+      const decodedIdToken = await firebaseAdmin.auth().verifyIdToken(idToken);
       return decodedIdToken.uid;
     } catch (error) {
       throw new AuthenticationError("Invalid ID token.");
