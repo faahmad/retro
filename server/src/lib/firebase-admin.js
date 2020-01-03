bd8a54e8ff85579603df037fee1106e297ca5daa
faraz ahmad <farazamiruddin@gmail.com>
2020-01-03 14:41:29 -0500

    Fix typo related to service account key

--- server/src/lib/firebase-admin.js
+++ server/src/lib/firebase-admin.js
@@ -1,6 +1,6 @@
 import admin from "firebase-admin";
 import devServiceAccountKey from "../config/serviceAccountKey.dev.json";
-import prodServiceAccountKey from "../config/serviceAccountKey.dev.json";
+import prodServiceAccountKey from "../config/serviceAccountKey.prod.json";
 
 const serviceAccountKey =
   process.env.NODE_ENV === "production"
