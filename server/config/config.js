b9d21844e2426f8efb0718f71adb93766bed21f4
faraz ahmad <farazamiruddin@gmail.com>
2020-01-10 13:21:54 -0500

    Refactor sequelize config

--- server/config/config.js
+++ server/config/config.js
@@ -1,22 +1,28 @@
 require("dotenv").config();
 
+const nonProductionOptions = {
+  username: "me",
+  database: "retro_dev",
+  host: "127.0.0.1",
+  dialect: "postgres"
+};
+const productionInstancePath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+
 module.exports = {
   development: {
-    username: "me",
-    database: "retro_dev",
-    host: "127.0.0.1",
-    dialect: "postgres"
+    ...nonProductionOptions
   },
   test: {
-    username: "me",
-    database: "retro_test",
-    host: "127.0.0.1",
-    dialect: "postgres"
+    ...nonProductionOptions
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+    host: productionInstancePath,
+    dialectOptions: {
+      socketPath: productionInstancePath
+    },
     dialect: "postgres"
   }
 };
