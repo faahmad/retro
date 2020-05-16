009832e17b678adb304034495461ee168a9bea06
Faraz Ahmad <faraz.ahmad@uber.com>
2020-05-16 14:57:39 -0400

    Setting up Stripe Checkout

---
+++ web/src/components/UpgradeToProBanner.tsx
@@ -0,0 +1,63 @@
+import * as React from "react";
+import moment from "moment";
+import { PawIcon } from "../images/PawIcon";
+import { Button } from "./Button";
+import { createStripeCheckoutSession } from "../services/stripe";
+import { loadStripe } from "@stripe/stripe-js";
+
+const stripePromise = loadStripe(
+  process.env.REACT_APP_STRIPE_PUBLISHABLE_KEY || ""
+);
+// Idle => Creating Checkout Session => Redirecting to Stripe Checkout => Success
+
+interface UpgradeToProBannerProps {
+  trialEnd: number;
+  workspaceId: string;
+}
+export const UpgradeToProBanner: React.FC<UpgradeToProBannerProps> = ({
+  trialEnd,
+  workspaceId,
+}) => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  // const [errorMessage, setErrorMessage] = React.useState("");
+
+  const handleOpenStripeCheckSession = async () => {
+    setIsLoading(true);
+    const { data } = await createStripeCheckoutSession({
+      workspaceId,
+      successUrl: `http://localhost:3000/workspaces/${workspaceId}`,
+      cancelUrl: `http://localhost:3000/workspaces/${workspaceId}`,
+    });
+    const sessionId = data.id;
+    const stripe = await stripePromise;
+    const response = await stripe?.redirectToCheckout({
+      sessionId,
+    });
+    console.log(response);
+    setIsLoading(false);
+    return;
+  };
+
+  return (
+    <div className="flex justify-between items-center text-blue my-2 p-4 bg-white border shadow flex-wrap">
+      <div className="flex flex-wrap items-center">
+        <PawIcon />
+        <div className="pl-2">
+          <p className="font-black">Upgrade to PRO</p>
+          <p className="text-sm">
+            Your free trial ends {moment.unix(trialEnd).fromNow()}. Upgrade to
+            PRO to keep leveling up your team.
+          </p>
+        </div>
+      </div>
+      <Button
+        className="text-red"
+        onClick={handleOpenStripeCheckSession}
+        disabled={isLoading}
+        style={{ maxWidth: "8rem" }}
+      >
+        {isLoading ? "Loading" : "Upgrade"}
+      </Button>
+    </div>
+  );
+};
