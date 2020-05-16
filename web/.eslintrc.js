b9ba77e6865722d8f00250fe8602cab09d0cd384
JovinCW <jovin@jovincron.in>
2020-05-16 14:39:30 -0700

    Displaying some rules for now

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -19,7 +19,9 @@ module.exports = {
         "plugin:prettier/recommended" // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     ],
     rules: {
-        // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
-        // e.g. "@typescript-eslint/explicit-function-return-type": "off",
+        "react/prop-types": 0,
+        "react/no-unescaped-entities": 0
+            // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+            // e.g. "@typescript-eslint/explicit-function-return-type": "off",
     },
 };
