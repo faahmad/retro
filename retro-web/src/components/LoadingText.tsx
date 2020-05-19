4bc4ba3008bbe5d2fccad3e43589525c1631f85d
Faraz Ahmad <faraz.ahmad@uber.com>
2020-05-19 13:08:46 -0400

    Delete legacy code

--- retro-web/src/components/LoadingText.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-export const LoadingText: React.FC = () => (
-  <div className="d-flex justify-content-center align-items-center text-muted font-weight-light">
-    Loading...
-  </div>
-);
