10ed75ce8572b9f22b7b7b8b278822af500e1e91
Faraz Ahmad <faraz.ahmad@uber.com>
2020-05-19 15:37:24 -0400

    testing lint staged

--- web/src/lib/axios.ts
+++ web/src/lib/axios.ts
@@ -3,6 +3,4 @@ import Axios from "axios";
 const idToken = localStorage.getItem("idToken");
 Axios.defaults.headers.common["x-retro-auth"] = idToken;
 
-export const testingOutHusky = {};
-
 export const axios = Axios;
