e2b5c264b2013025767b1fb3c8e348313d76a574
Faraz Ahmad <faraz.ahmad@uber.com>
2020-05-21 01:13:50 -0400

    Add workspace data to Airtable

--- firebase-functions/functions/src/handlers/workspaces/on-create/index.ts
+++ firebase-functions/functions/src/handlers/workspaces/on-create/index.ts
@@ -1,3 +1,4 @@
+export * from "./add-workspace-to-airtable";
 export * from "./create-stripe-customer-subscription";
 export * from "./create-workspace-url";
 export * from "./create-workspace-users";
