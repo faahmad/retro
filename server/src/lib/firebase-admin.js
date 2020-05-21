05b73314aad8c47e4539efcb34331db67c0a4894
faraz ahmad <faahmad@users.noreply.github.com>
2020-05-21 14:59:40 -0400

    Update env variables for service account (#94)

--- server/src/lib/firebase-admin.js
+++ server/src/lib/firebase-admin.js
@@ -1,26 +1,8 @@
-import admin from "firebase-admin";
+import * as admin from "firebase-admin";
 
-const B64_DEV_SERVICE_ACCOUNT_KEY = process.env.B64_DEV_SERVICE_ACCOUNT_KEY
-const devServiceAccountKey = Buffer.from(B64_DEV_SERVICE_ACCOUNT_KEY, 'base64').toString('utf-8');
-const devSAK = JSON.parse(devServiceAccountKey)
-
-const B64_PROD_SERVICE_ACCOUNT_KEY = process.env.B64_PROD_SERVICE_ACCOUNT_KEY
-const prodServiceAccountKey = Buffer.from(B64_PROD_SERVICE_ACCOUNT_KEY, 'base64').toString('utf-8');
-const prodSAK = JSON.parse(prodServiceAccountKey)
-
-function getServiceAccountKey(env) {
-    switch (env) {
-        case "production":
-            return prodSAK;
-        case "development":
-            return devSAK;
-        case "test":
-            return devSAK;
-    }
-}
-
-const serviceAccountKey = getServiceAccountKey(process.env.NODE_ENV);
-
+const serviceAccountKey = JSON.parse(
+  Buffer.from(process.env.B64_SERVICE_ACCOUNT_KEY, "base64").toString("utf-8")
+);
 admin.initializeApp({ credential: admin.credential.cert(serviceAccountKey) });
 
 export default admin;
