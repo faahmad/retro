95d4c08f50261037e0b5aa5ff5d0a26ca24e9e61
JovinCW <jovin@jovincron.in>
2020-05-22 12:24:15 -0700

    Making another push to fix icons

--- server/src/lib/firebase-admin.js
+++ server/src/lib/firebase-admin.js
@@ -1,7 +1,7 @@
 import * as admin from "firebase-admin";
 
 const serviceAccountKey = JSON.parse(
-  Buffer.from(process.env.B64_SERVICE_ACCOUNT_KEY, "base64").toString("utf-8")
+    Buffer.from(process.env.B64_SERVICE_ACCOUNT_KEY, "base64").toString("utf-8")
 );
 admin.initializeApp({ credential: admin.credential.cert(serviceAccountKey) });
 
