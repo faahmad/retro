f70fb5307970a91aae634ea8d5e77373ff43aaba
Faraz Ahmad <faraz.ahmad@uber.com>
2020-05-25 13:48:00 -0400

    create getStripeSubscriptionStatus cloud function

--- firebase-functions/functions/src/handlers/https/on-request/get-stripe-subscription-status.ts
+++ firebase-functions/functions/src/handlers/https/on-request/get-stripe-subscription-status.ts
@@ -3,5 +3,35 @@ import {
   getUserIdFromIdToken,
   getWorkspaceUsers
 } from "../../../services/firebase-admin";
+import { getStripeSubscription } from "../../../services/stripe";
 import { cors } from "../../../lib/cors";
 import { logger } from "../../../lib/logger";
+import { has } from "lodash";
+
+export const getStripeSubscriptionStatus = functions.https.onRequest((req, res) => {
+  cors(req, res, async () => {
+    try {
+      const idToken = req.headers["x-retro-auth"];
+      if (!idToken || typeof idToken !== "string") {
+        throw new Error("Invalid Auth Header.");
+      }
+
+      const { workspaceId } = req.body;
+      if (!workspaceId) {
+        throw new Error("Invalid Request Body.");
+      }
+
+      const userId = await getUserIdFromIdToken(idToken);
+      const workspaceUsers = await getWorkspaceUsers(workspaceId);
+      if (!workspaceUsers || !has(workspaceUsers.users, userId)) {
+        throw new Error("Unauthorized.");
+      }
+
+      const subscription = await getStripeSubscription(workspaceId);
+      return res.status(200).json({ status: subscription.status });
+    } catch ({ message }) {
+      logger.log(message);
+      return res.status(500).json({ error: message });
+    }
+  });
+});
