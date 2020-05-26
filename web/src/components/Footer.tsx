b18e426b322fc32174ece5720462141e745ab28c
Faraz Ahmad <faraz.ahmad@uber.com>
2020-05-26 16:28:34 -0400

    add privacy and tos to footer

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -1,11 +1,20 @@
 import * as React from "react";
 import dashboardFooterImage from "../assets/images/dashboard-footer-image.svg";
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
     <React.Fragment>
       <img className="w-full" src={dashboardFooterImage} alt="Footer Illustration" />
-      <footer className="bg-pink text-blue p-2 text-center">
+      <footer className="bg-pink text-blue text-xs p-2 flex justify-between">
+        <div className="flex">
+          <Link to="/terms" className="px-2">
+            Terms of Service
+          </Link>
+          <Link to="/privacy" className="px-2">
+            Privacy Policy
+          </Link>
+        </div>
         <p className="text-blue">&copy; 2020, Retro Technology</p>
       </footer>
     </React.Fragment>
