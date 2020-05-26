d62908dadafed947167583670bc0c0cbf3697c4e
Faraz Ahmad <faraz.ahmad@uber.com>
2020-05-26 16:19:17 -0400

    Add tos page

--- web/src/AppRoutes.tsx
+++ web/src/AppRoutes.tsx
@@ -13,6 +13,8 @@ import { useQuery } from "@apollo/react-hooks";
 import { LoadingText } from "./components/LoadingText";
 import { RetroBoardPage } from "./pages/RetroBoardPage";
 import { SettingsPage } from "./pages/SettingsPage";
+import { PrivacyPolicyPage } from "./pages/PrivacyPolicyPage";
+import { TermsOfServicePage } from "./pages/TermsOfServicePage";
 import { SubscriptionStatusProvider } from "./contexts/SubscriptionStatusContext";
 
 const optimizely = createInstance({
@@ -42,6 +44,10 @@ export const AppRoutes: React.FC = () => {
           <Navbar />
         </div>
         <Switch>
+          <Route exact path="/privacy" component={PrivacyPolicyPage} />
+          <Route exact path="/terms" component={TermsOfServicePage} />
+          <Route exact path="/faq" component={FAQPage} />
+          <Route exact path="/design" component={DesignPage} />
           {authAccount ? <AuthenticatedAppRoutes /> : <UnauthenticatedAppRoutes />}
         </Switch>
       </BrowserRouter>
@@ -53,8 +59,6 @@ const UnauthenticatedAppRoutes: React.FC = () => {
   return (
     <React.Fragment>
       <Route exact path="/" component={LandingPage} />
-      <Route exact path="/faq" component={FAQPage} />
-      <Route exact path="/design" component={DesignPage} />
     </React.Fragment>
   );
 };
