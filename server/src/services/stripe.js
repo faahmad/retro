b30753013741ed7bc3c64351ec17253146093dd8
Faraz Ahmad <faraz.ahmad@uber.com>
2020-05-26 16:31:05 -0400

    remove console.log

--- server/src/services/stripe.js
+++ server/src/services/stripe.js
@@ -3,7 +3,6 @@ import { get } from "lodash";
 
 export async function getStripeCustomer(customerId) {
   const customer = await stripe.customers.retrieve(customerId);
-  console.log(JSON.stringify(customer, null, 2));
   return {
     id: customer.id,
     defaultPaymentMethod: get(customer, "invoice_settings.default_payment_method", null)
