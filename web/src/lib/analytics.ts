0ce685efe153adbec0990a48bf12bba341dffc74
Faraz Ahmad <faraz.ahmad@uber.com>
2021-01-01 14:40:50 -0500

    add ga tracking id

--- web/src/lib/analytics.ts
+++ web/src/lib/analytics.ts
@@ -2,6 +2,10 @@ import analytics from "analytics.js";
 
 if (process.env.NODE_ENV !== "test") {
   analytics.initialize({
+    "Google Analytics": {
+      trackingId: "G-1H9PFF6L2E",
+      sendUserId: true
+    },
     Amplitude: {
       apiKey: process.env.REACT_APP_AMPLITUDE_API_KEY,
       trackAllPages: true,
