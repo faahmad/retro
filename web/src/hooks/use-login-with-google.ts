894ca368e6faaf8c3e3975b055b81063928fda87
Faraz Ahmad <faraz.ahmad@uber.com>
2021-01-11 21:07:09 -0500

    delete gql mutation createUser

--- web/src/hooks/use-login-with-google.ts
+++ web/src/hooks/use-login-with-google.ts
@@ -1,43 +1,23 @@
-import { gql } from "apollo-boost";
 import analytics from "analytics.js";
 import {
   authenticateWithGoogle,
   getIdTokenFromUserCredential,
   isNewUser
 } from "../services/auth-service";
-import { useMutation } from "@apollo/react-hooks";
 import { useHistory } from "react-router-dom";
 
-const CREATE_USER_MUTATION = gql`
-  mutation CreateUserMutation($input: CreateUserInput!) {
-    createUser(input: $input) {
-      id
-      email
-      createdAt
-    }
-  }
-`;
-
 export function useLoginWithGoogle() {
-  const [createUser] = useMutation(CREATE_USER_MUTATION);
   const history = useHistory();
 
   const handleLoginWithGoogle = async () => {
     const userCredential = await authenticateWithGoogle();
-    const idToken = await getIdTokenFromUserCredential(userCredential);
     if (isNewUser(userCredential)) {
-      handleNewUser(userCredential, idToken);
+      handleNewUser(userCredential);
     }
     return;
   };
 
-  const handleNewUser = async (userCredential: any, idToken: string) => {
-    await createUser({
-      context: { idToken },
-      variables: {
-        input: { id: userCredential.user.uid, email: userCredential.user.email }
-      }
-    });
+  const handleNewUser = async (userCredential: any) => {
     analytics.identify(userCredential?.user?.uid);
     analytics.track("User Signed Up", {
       type: "organic",
