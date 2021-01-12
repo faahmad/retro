4d0715692beed32a1ffecc7b57d47064de32f7fb
Faraz Ahmad <faraz.ahmad@uber.com>
2021-01-12 13:55:59 -0500

    update to latest stripe api

--- web/functions/src/lib/stripe.ts
+++ web/functions/src/lib/stripe.ts
@@ -2,5 +2,5 @@ import * as functions from "firebase-functions";
 import Stripe from "stripe";
 
 export const stripe = new Stripe(functions.config().stripe.secret_key, {
-  apiVersion: "2020-03-02"
+  apiVersion: "2020-08-27"
 });
