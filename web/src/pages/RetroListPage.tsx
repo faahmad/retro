bb02eff3f96028fbc5872a70f9e83f738c5090a7
Faraz Ahmad <faraz.ahmad@uber.com>
2021-02-08 22:08:12 -0500

    Create RetroListPage.tsx

---
+++ web/src/pages/RetroListPage.tsx
@@ -0,0 +1,35 @@
+import * as React from "react";
+import { PageContainer } from "../components/PageContainer";
+import { useWorkspaceState } from "../hooks/use-workspace-state";
+import { Retro } from "../types/retro";
+import { useHistory, useParams } from "react-router-dom";
+import { Workspace } from "../types/workspace";
+import { useGetWorkspaceRetros } from "../hooks/use-get-workspace-retros";
+
+export function RetroListPage() {
+  const history = useHistory();
+  const handleRedirectToRetroPage = (retro: Retro) => {
+    // analytics.track("Retro Opened", { ...retro });
+    return history.push(`/workspaces/${retro.workspaceId}/retros/${retro.id}`);
+  };
+  const workspaceState = useWorkspaceState();
+  const { getWorkspaceRetros } = useGetWorkspaceRetros();
+
+  return (
+    <PageContainer>
+      <p className="text-blue mb-2 underline">{workspaceState.name}</p>
+      <h1 className="text-blue font-black text-3xl">All Retros</h1>
+      <div className="flex flex-wrap">
+        {retros.map((retro) => {
+          return (
+            <RetroCard
+              key={retro.id}
+              retro={retro}
+              onClick={() => handleRedirectToRetroPage(retro)}
+            />
+          );
+        })}
+      </div>
+    </PageContainer>
+  );
+}
