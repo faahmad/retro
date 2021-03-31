c5c5befe9224b4d4eb3dac14ec1b4a0eb7b99f19
faraz ahmad <faahmad@users.noreply.github.com>
2021-03-30 21:57:02 -0400

    Default workspaces to yearly subscription (#183)

--- web/functions/src/constants/stripe.ts
+++ web/functions/src/constants/stripe.ts
@@ -3,10 +3,12 @@ import { projectIds } from "./project-ids";
 const isProd = process.env.GCLOUD_PROJECT === projectIds.prod;
 
 enum DevStripeSubscriptionPlans {
-  PRO = "plan_HFppfQJn3jDBFj"
+  PRO_MONTHLY = "plan_HFppfQJn3jDBFj",
+  PRO_YEARLY = "price_1Iat06KZLl59Zw3Xr6NBJHvQ"
 }
 enum ProdStripeSubscriptionPlans {
-  PRO = "plan_HGB5FTkzgHuvyk"
+  PRO_MONTHLY = "plan_HGB5FTkzgHuvyk",
+  PRO_YEARLY = "price_1Ias3mKZLl59Zw3X4RclJTyN"
 }
 export const StripeSubscriptionPlans = isProd
   ? ProdStripeSubscriptionPlans
