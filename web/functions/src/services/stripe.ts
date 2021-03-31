39a60dfe505e14e4597a4a540ffd4e4d7c0fa98d
Faraz Ahmad <faraz.ahmad@uber.com>
2021-03-30 20:20:10 -0400

    create stripeWebhook cloud function

--- web/functions/src/services/stripe.ts
+++ web/functions/src/services/stripe.ts
@@ -34,25 +34,34 @@ export function createBillingPortalSession({
   return stripe.billingPortal.sessions.create(params);
 }
 
-// export async function getStripeSubscription(subscriptionId: Stripe.Subscription["id"]) {
-//   const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+export async function getStripeSubscription(subscriptionId: Stripe.Subscription["id"]) {
+  const subscription = await stripe.subscriptions.retrieve(subscriptionId);
+  return {
+    id: subscription.id,
+    customerId: subscription.customer,
+    status: subscription.status,
+    items: subscription.items.data.map(getSubscriptionItem),
+    trialStart: subscription.trial_start,
+    trialEnd: subscription.trial_end,
+    startDate: subscription.start_date,
+    currentPeriodStart: subscription.current_period_start,
+    currentPeriodEnd: subscription.current_period_end
+  };
+}
 
-//   return {
-//     id: subscription.id,
-//     customerId: subscription.customer,
-//     status: subscription.status,
-//     plan: {
-//       id: subscription?.plan?.id,
-//       active: subscription?.plan?.active,
-//       amount: subscription?.plan?.amount,
-//       currency: subscription?.plan?.currency,
-//       interval: subscription?.plan?.interval,
-//       productId: subscription?.plan?.product
-//     },
-//     trialStart: subscription.trial_start,
-//     trialEnd: subscription.trial_end,
-//     startDate: subscription.start_date,
-//     currentPeriodStart: subscription.current_period_start,
-//     currentPeriodEnd: subscription.current_period_end
-//   };
-// }
+// Private functions.
+function getSubscriptionItem(item: Stripe.SubscriptionItem) {
+  return {
+    id: item.id,
+    subscriptionId: item.subscription,
+    created: item.created,
+    type: item.price.type,
+    active: item.price.active,
+    amount: item.price.unit_amount,
+    currency: item.price.currency,
+    liveMode: item.price.livemode,
+    product: item.price.product,
+    interval: item.price.recurring?.interval,
+    intervalCount: item.price.recurring?.interval_count
+  };
+}
