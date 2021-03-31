b89934800ba1cb6e4fbc15b619d1a3a0d47bfc3d
Faraz Ahmad <faraz.ahmad@uber.com>
2021-03-30 20:20:32 -0400

    update Workspace type

--- web/functions/src/types/workspace.ts
+++ web/functions/src/types/workspace.ts
@@ -1,12 +1,20 @@
 export interface Workspace {
+  id: string;
   name: string;
   url: string;
   allowedEmailDomains: string[];
   ownerId: string;
   ownerEmail: string;
-  createdAt: string;
-  updatedAt: string;
+  createdAt: any;
+  updatedAt: any;
   subscriptionStatus: string;
   subscriptionTrialEnd: number | null;
+  retroItemsData: {
+    goodCount: number;
+    badCount: number;
+    actionsCount: number;
+    questionsCount: number;
+  };
   customerId?: string;
+  subscriptionId?: string;
 }
