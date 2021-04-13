285961d39ab270a5b138d202671d9971387c1319
Faraz Ahmad <faraz.ahmad@uber.com>
2021-04-13 17:21:25 -0400

    add MagicLink login to login page

--- web/src/pages/LoginPage.tsx
+++ web/src/pages/LoginPage.tsx
@@ -14,6 +14,26 @@ export function LoginPage() {
   useAnalyticsPage(AnalyticsPage.LOGIN);
   const [input, setInput] = React.useState("");
 
+  return (
+    <PageContainer>
+      <form className="flex flex-col items-center mb-16">
+        <label className="text-blue" htmlFor="secure-login">
+          Secure login
+        </label>
+        <input
+          placeholder="What's the password?"
+          className="text-blue border border-red my-1 h-12 sm:h-8 md:h-8 lg:h-8 w-full max-w-md outline-none px-1"
+          type="text"
+          name="secure-login"
+          onChange={(e) => setInput(e.target.value)}
+        />
+      </form>
+      {input === "CrowdFox" && <LoginFormContainer />}
+    </PageContainer>
+  );
+}
+
+export function LoginFormContainer({ onSuccess, title }: any) {
   const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
   const [success, setSuccess] = React.useState(false);
   const [isSending, setIsSending] = React.useState(false);
@@ -51,45 +71,51 @@ export function LoginPage() {
 
   return (
     <PageContainer>
-      <form className="flex flex-col items-center mb-16">
-        <label className="text-blue" htmlFor="secure-login">
-          Secure login
-        </label>
-        <input
-          placeholder="What's the password?"
-          className="text-blue border border-red my-1 h-12 sm:h-8 md:h-8 lg:h-8 w-full max-w-md outline-none px-1"
-          type="text"
-          name="secure-login"
-          onChange={(e) => setInput(e.target.value)}
+      {errorMessage && <ErrorMessageBanner message={errorMessage} />}
+
+      <LoginForm
+        title={title}
+        isSending={isSending}
+        onSubmit={handleSubmit}
+        onSuccess={onSuccess}
+      />
+      {success && (
+        <NotificationBanner
+          title="Success!"
+          message="A login link has been sent to your email."
         />
-      </form>
-      {input === "CrowdFox" && <LoginFormContainer errorMessage={errorMessage} isSending={isSending} handleSubmit={handleSubmit} success={success} />}
+      )}
     </PageContainer>
   );
 }
 
-export function Login({ title = "Log In" }: { title?: string }) {
+// Private components.
+function LoginForm({ isSending, onSubmit, title = "Sign in (or up)" }: any) {
   const loginWithGoogle = useLoginWithGoogle();
   return (
-    <div className="flex flex-col items-center lg:text-center m-0">
-      <h1 className="text-blue text-3xl mb-4">{title}</h1>
+    <div className="flex flex-col items-center">
+      <h1 className="text-blue text-3xl mb-6 text-center">{title}</h1>
       <div className="text-blue text-red">
         <GoogleOAuthButton onClick={loginWithGoogle}>Continue with</GoogleOAuthButton>
       </div>
-      <div className="mt-8 text-pink sm:w-full lg:w-2/5 text-center text-xs">
-        <p style={{ color: "rgba(55, 53, 47, 0.4)" }}>
-          By creating an account, you acknowledge that you have read and understood, and
-          agree to Retro's{" "}
-          <Link to="/terms" className="underline">
-            Terms & Conditions
-          </Link>{" "}
-          and{" "}
-          <Link to="/privacy" className="underline">
-            Privacy Policy
-          </Link>
-          .
-        </p>
-      </div>
+      <div className="text-blue my-8">or</div>
+      <form className="flex flex-col mb-12" onSubmit={onSubmit}>
+        <div className="flex flex-col mb-4">
+          <label className="text-blue text-sm" htmlFor="email">
+            Work email
+          </label>
+          <input
+            type="email"
+            name="email"
+            placeholder="you@company.com"
+            className="text-blue border border-red my-1 h-12 sm:h-8 md:h-8 lg:h-8 w-full max-w-md outline-none px-1"
+          />
+        </div>
+        <Button disabled={isSending} className="text-red" type="submit">
+          {isSending ? "Sending..." : "Email a login link"}
+        </Button>
+      </form>
+      <TermsAndConditionsText />
     </div>
   );
 }
@@ -112,4 +138,3 @@ function TermsAndConditionsText() {
     </div>
   );
 }
-
