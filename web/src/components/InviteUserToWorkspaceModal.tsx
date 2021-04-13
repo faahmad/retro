e6beb8fa4aa52442f8ebd4ada7aa72ef892bcfe1
Faraz Ahmad <faraz.ahmad@uber.com>
2021-04-13 17:23:03 -0400

    add invite link to inviteusermodal

--- web/src/components/InviteUserToWorkspaceModal.tsx
+++ web/src/components/InviteUserToWorkspaceModal.tsx
@@ -7,6 +7,7 @@ import * as yup from "yup";
 import { useCreateWorkspaceInvite } from "../hooks/use-create-workspace-invite";
 import { useAnalyticsEvent, AnalyticsEvent } from "../hooks/use-analytics-event";
 import { AnalyticsPage } from "../hooks/use-analytics-page";
+import * as Sentry from "@sentry/react";
 interface InviteUserToWorkspaceModalProps {
   isOpen: boolean;
   onRequestClose: (
@@ -18,6 +19,7 @@ interface InviteUserToWorkspaceModalProps {
   workspaceOwnerId: string;
   userCount: number;
   invitedUserCount: number;
+  workspaceURL: string;
 }
 
 export const InviteUserToWorkspaceModal: React.FC<InviteUserToWorkspaceModalProps> = ({
@@ -27,23 +29,68 @@ export const InviteUserToWorkspaceModal: React.FC<InviteUserToWorkspaceModalProp
   workspaceId,
   workspaceName,
   workspaceOwnerId,
+  workspaceURL,
   userCount,
   invitedUserCount
 }) => {
+  const trackEvent = useAnalyticsEvent();
+  const [copyButtonText, setCopyButtonText] = React.useState("Copy");
+  const inviteLink = `${window.location.origin}/join/${workspaceURL}`;
+  const handleCopyToClipboard = async () => {
+    try {
+      await navigator.clipboard.writeText(inviteLink);
+      setCopyButtonText("Copied!");
+      setTimeout(() => {
+        setCopyButtonText("Copy");
+      }, 2000);
+      trackEvent(AnalyticsEvent.INVITE_LINK_COPIED, {
+        inviteLink,
+        location: AnalyticsPage.DASHBOARD,
+        component: "InviteUserToWorkspaceModal",
+        variant: "both"
+      });
+      return;
+    } catch (error) {
+      Sentry.captureException(error);
+      setCopyButtonText("Try again");
+    }
+  };
+
   return (
     <ReactModal
       ariaHideApp={false}
       isOpen={isOpen}
       onRequestClose={onRequestClose}
       style={{
-        content: { maxWidth: "420px", height: "530px", padding: "20px" },
+        content: { maxWidth: "600px", maxHeight: "640px", padding: "20px" },
         overlay: { background: "rgba(17, 38, 156, 0.6)" }
       }}
       className="bg-white shadow-red border m-auto absolute inset-0 border-red focus:outline-none z-50"
       // IMPORTANT: closeTimeoutMS has to be the same as what is set in the tailwind.css file.
       closeTimeoutMS={200}
     >
-      <img className="w-full" src={addTeamMemberImage} alt="Add your team member" />
+      <div className="flex items-center justify-center">
+        <img src={addTeamMemberImage} alt="Add your team member" />
+      </div>
+      <div className="flex flex-col p-4">
+        <div className="flex justify-between items-end w-full text-blue py-1">
+          <p className="text-xl font-black">Invite Link</p>
+        </div>
+        <p className="text-blue text-xs">
+          Share this link with anyone you'd like to join this workspace.
+        </p>
+        <div className="flex justify-between items-center mt-4">
+          <p className="flex-grow text-blue text-xs">{inviteLink}</p>
+          <Button
+            style={{ width: "11rem" }}
+            className="text-white bg-blue border-red shadow-red self-center"
+            onClick={handleCopyToClipboard}
+          >
+            {copyButtonText}
+          </Button>
+        </div>
+      </div>
+      <div className="text-blue text-center my-2">or</div>
       <InviteUserToWorkspaceForm
         workspaceOwnerId={workspaceOwnerId}
         workspaceId={workspaceId}
@@ -117,38 +164,29 @@ const InviteUserToWorkspaceForm: React.FC<{
 
   return (
     <form onSubmit={formik.handleSubmit}>
-      <div className="p-4 mt-4 text-blue">
-        <p className="font-black">Add your team member</p>
-        <div className="my-4">
-          <input
-            id="email"
-            name="email"
-            placeholder="example@email.com"
-            type="text"
-            value={formik.values.email}
-            onChange={formik.handleChange}
-            className="border border-red h-8 w-full max-w-md px-1"
-          />
-          {formik.errors.email && formik.touched.email && (
-            <p className="text-xs text-red">{formik.errors.email}</p>
-          )}
-        </div>
-        <div className="flex align-center justify-between mt-8">
-          {/* 
-            FIXME: 2/18/2020 
-            Tried to override the styles by passing in classNames,
-            but it wasn't working. Decided to use inline for time's sake,
-            however, we should be able to override any styles via the className prop.  
-          */}
+      <div className="px-4 text-blue">
+        <p className="font-black">Email an Invite</p>
+        <div className="my-2 flex justify-around items-center mr-2">
+          <div className="flex-grow">
+            <input
+              id="email"
+              name="email"
+              placeholder="example@email.com"
+              type="text"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              className="border border-red h-8 w-full px-1"
+            />
+            {formik.errors.email && formik.touched.email && (
+              <p className="text-xs text-red">{formik.errors.email}</p>
+            )}
+          </div>
           <Button
-            onClick={onClick}
+            className="ml-4"
+            style={{ width: "10rem" }}
+            type="submit"
             disabled={isDisabled}
-            className="text-red border-none shadow-none"
-            style={{ width: "6rem", boxShadow: "none" }}
           >
-            Cancel
-          </Button>
-          <Button style={{ width: "10rem" }} type="submit" disabled={isDisabled}>
             {submitButtonText}
           </Button>
         </div>
