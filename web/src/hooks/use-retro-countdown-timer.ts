e2dfe12d665094c2dd82f122367d2ebba8b14df5
Faraz Ahmad <faraz.ahmad@uber.com>
2021-08-16 15:23:32 -0400

    Create use-retro-countdown-timer.ts

---
+++ web/src/hooks/use-retro-countdown-timer.ts
@@ -0,0 +1,190 @@
+import * as React from "react";
+import firebase from "../lib/firebase";
+import { Retro, RetroChildRef } from "../types/retro";
+import { RealtimeDatabaseRefs } from "../constants/realtime-database-refs";
+
+type CountdownTimerT = RetroChildRef["countdownTimer"];
+type CountdownTimerUpdateFieldT = { key: keyof CountdownTimerT; value: any };
+type PartialCountdownTimerT = Partial<CountdownTimerT>;
+
+enum CountdownTimerState {
+  CLOSED = "CLOSED",
+  PAUSED = "PAUSED",
+  COUNTING = "COUNTING"
+}
+
+export type RetroCountdownTimerT = {
+  isTimerOpen: boolean | null;
+  toggleTimer: () => Promise<void>;
+  timeLeft: number;
+  isPaused: boolean;
+  start: () => void;
+  pause: () => void;
+  setTime: (milliseconds: number) => void;
+  reset: () => void;
+  add1Min: () => void;
+};
+
+export function useRetroCountdownTimer(retroId: Retro["id"]): RetroCountdownTimerT {
+  const [countdownTimer, setCountdownTimer] = React.useState<CountdownTimerT | null>(
+    null
+  );
+  const serverTimeOffset = useServerTimeOffset();
+  const [timeLeft, setTimeLeft] = React.useState(0);
+  const initialTime = 600_000;
+
+  React.useEffect(() => {
+    const ref = getRetroRef(retroId);
+    if (ref) {
+      ref.on("value", (snapshot) => {
+        const data = snapshot.val();
+        setCountdownTimer(data.countdownTimer);
+      });
+      return () => ref.off();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [retroId]);
+
+  const handleUpdate = async (fields: CountdownTimerUpdateFieldT[]) => {
+    const updates: PartialCountdownTimerT = fields.reduce((object: any, field) => {
+      object[`countdownTimer/${field.key}`] = field.value;
+      return object;
+    }, {});
+    await updateCountdownTimer(retroId, updates);
+    return;
+  };
+
+  React.useEffect(() => {
+    const initialzeTimer = () => {
+      handleUpdate([
+        { key: "milliseconds", value: initialTime },
+        { key: "state", value: CountdownTimerState.CLOSED }
+      ]);
+    };
+    initialzeTimer();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  let milliseconds = countdownTimer?.milliseconds;
+  React.useEffect(() => {
+    if (milliseconds) {
+      setTimeLeft(milliseconds);
+    }
+  }, [milliseconds]);
+
+  const _getServerTimestamp = () => {
+    return firebase.database.ServerValue.TIMESTAMP;
+  };
+
+  const handleChangeTime = (milliseconds: number) => {
+    handleUpdate([{ key: "milliseconds", value: milliseconds }]);
+    return;
+  };
+
+  const handleAdd1Min = () => {
+    handleChangeTime((countdownTimer?.milliseconds || 0) + 60_000);
+    return;
+  };
+
+  const handleResetTime = () => {
+    handleChangeTime(initialTime);
+    return;
+  };
+
+  const handleStartTimer = () => {
+    handleUpdate([
+      { key: "state", value: CountdownTimerState.COUNTING },
+      { key: "startAt", value: _getServerTimestamp() }
+    ]);
+    return;
+  };
+
+  const handlePauseTimer = () => {
+    handleUpdate([
+      { key: "state", value: CountdownTimerState.PAUSED },
+      { key: "milliseconds", value: timeLeft > 0 ? timeLeft : 0 }
+    ]);
+    return;
+  };
+
+  const intervalRef = useInterval(
+    () => {
+      const diff = Date.now() - countdownTimer?.startAt - serverTimeOffset;
+      setTimeLeft((countdownTimer?.milliseconds || 0) - diff);
+      if (timeLeft < 0) {
+        window.clearInterval(intervalRef.current);
+        handleChangeTime(0);
+        handlePauseTimer();
+      }
+    },
+    countdownTimer?.state === CountdownTimerState.COUNTING ? 100 : null
+  );
+
+  const isOpen = countdownTimer && countdownTimer.state !== CountdownTimerState.CLOSED;
+  const handleToggleTimer = async () => {
+    const nextState = isOpen ? CountdownTimerState.CLOSED : CountdownTimerState.PAUSED;
+    await handleUpdate([{ key: "state", value: nextState }]);
+    return;
+  };
+
+  return {
+    isTimerOpen: isOpen,
+    toggleTimer: handleToggleTimer,
+    /**
+     * The amount of time remaining in milliseconds.
+     */
+    timeLeft,
+    isPaused: countdownTimer?.state === CountdownTimerState.PAUSED,
+    start: handleStartTimer,
+    pause: handlePauseTimer,
+    setTime: handleChangeTime,
+    reset: handleResetTime,
+    add1Min: handleAdd1Min
+  };
+}
+
+// Helpers
+const _retroRef = firebase.database().ref(RealtimeDatabaseRefs.RETRO);
+function getRetroRef(retroId: string) {
+  return _retroRef.child(retroId);
+}
+function updateCountdownTimer(retroId: string, updates: PartialCountdownTimerT) {
+  return _retroRef.child(retroId).update(updates);
+}
+
+/**
+ * @returns The server time offset to use to calculate the difference
+ * between the server's time and the local client's time.
+ */
+function useServerTimeOffset() {
+  const [serverTimeOffset, setServerTimeOffset] = React.useState(0);
+
+  React.useEffect(() => {
+    const serverTimeOffsetRef = firebase.database().ref(".info/serverTimeOffset");
+    serverTimeOffsetRef.on("value", (snapshot) => setServerTimeOffset(snapshot.val()));
+    return () => serverTimeOffsetRef.off();
+  }, []);
+
+  return serverTimeOffset;
+}
+
+function useInterval(callback: () => void, delay: number | null) {
+  const intervalRef = React.useRef<any>(null);
+  const savedCallback = React.useRef(callback);
+
+  React.useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+
+  React.useEffect(() => {
+    // Bail early if delay is null.
+    if (delay === null) {
+      return;
+    }
+    const tick = () => savedCallback.current();
+    intervalRef.current = window.setInterval(tick, delay);
+    return () => window.clearInterval(intervalRef.current);
+  }, [delay]);
+
+  return intervalRef;
+}
