4f953f1d07d5bb00fd659cbcd90594d5d47bd2e2
fahmad@netflix.com <Faraz Ahmad@fahmadnxcomsmbp.myfiosgateway.com>
2022-02-03 20:56:00 -0500

    Setup dev env

--- web/src/lib/firebase.ts
+++ web/src/lib/firebase.ts
@@ -5,24 +5,24 @@ import "firebase/storage";
 import "firebase/analytics";
 import "firebase/database";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAQTaOwWXu7En7r-GnYI-cwWs9axCa-l3U",
-  authDomain: "retro-prod-786.firebaseapp.com",
-  databaseURL: "https://retro-prod-786.firebaseio.com",
-  projectId: "retro-prod-786",
-  storageBucket: "retro-prod-786.appspot.com",
-  messagingSenderId: "700582464458",
-  appId: "1:700582464458:web:37d77798d0d88574955d33",
-  measurementId: "G-8D2MQ5KNS5"
-};
-
-firebase.initializeApp(firebaseConfig);
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+});
 
 firebase.analytics();
 const database = firebase.database();
 const firestore = firebase.firestore();
 
-if (window.location.hostname === "localhost") {
+const IS_DEV = window.location.hostname === "localhost";
+if (IS_DEV) {
+  console.info("Localhost detected, using emulator.");
   firebase.auth().useEmulator("http://localhost:9099/");
   firestore.settings({
     host: "localhost:8080",
