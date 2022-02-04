a18745e0209a0c6be18d2ab61c6833a4c9ac1ff4
faraz ahmad <faahmad@users.noreply.github.com>
2022-02-03 19:50:44 -0500

    delete server code (#244)
    Co-authored-by: fahmad@netflix.com <Faraz Ahmad@fahmadnxcomsmbp.myfiosgateway.com>

--- server/src/lib/firebase-admin.js
+++
@@ -1,8 +0,0 @@
-import * as admin from "firebase-admin";
-
-const serviceAccountKey = JSON.parse(
-    Buffer.from(process.env.B64_SERVICE_ACCOUNT_KEY, "base64").toString("utf-8")
-);
-admin.initializeApp({ credential: admin.credential.cert(serviceAccountKey) });
-
-export default admin;
