4c2e3e15054cafe33d50691626ab1a37529dbbcc
faraz ahmad <faahmad@users.noreply.github.com>
2022-02-03 21:01:33 -0500

    Debug (#245)
    * first commit
    
    * Setup dev env
    
    Co-authored-by: fahmad@netflix.com <Faraz Ahmad@fahmadnxcomsmbp.myfiosgateway.com>

--- web/src/lib/firebase.ts
+++ web/src/lib/firebase.ts
@@ -8,11 +8,11 @@ import "firebase/database";
 firebase.initializeApp({
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 });
 
@@ -20,7 +20,9 @@ firebase.analytics();
 const database = firebase.database();
 const firestore = firebase.firestore();
 
-if (window.location.hostname === "localhost") {
+const IS_DEV = window.location.hostname === "localhost";
+if (IS_DEV) {
+  console.info("Localhost detected, using emulator.");
   firebase.auth().useEmulator("http://localhost:9099/");
   firestore.settings({
     host: "localhost:8080",
