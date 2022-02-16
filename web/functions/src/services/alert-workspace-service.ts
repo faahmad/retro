5ea812873cfcdedc6e2130bceeaa1314cfd64077
Josue Lugaro <josuelugaro15@gmail.com>
2022-02-16 15:24:11 -0600

    Added constants to store discord url info

--- web/functions/src/services/alert-workspace-service.ts
+++ web/functions/src/services/alert-workspace-service.ts
@@ -1,8 +1,12 @@
 import * as request from "request";
 
+const DISCORD_APP_URL = 'https://discordapp.com/api/webhooks';
+const DISCORD_NOTIFICATIONS_CHANNEL = '941806106638954506/0Exv2DN8kbzmTwj1vMOMQ8jsuFBtAjzmv7GPwLgVnmOJA61nElBiH8iJUzJerSoARfGR';
+const DISCORD_TEST_NOTIFICATIONS_CHANNEL = '940746404366794782/6cc1whDk-RMckJEktfTSoOSAuXtCgHjOf26yY-zlYc3SPNjBFlISLatkp5iCtxNbvE1c'
+
 export const publishMessage = (message: string) => {
   return request.post(
-    "https://discordapp.com/api/webhooks/941806106638954506/0Exv2DN8kbzmTwj1vMOMQ8jsuFBtAjzmv7GPwLgVnmOJA61nElBiH8iJUzJerSoARfGR",
+    `${DISCORD_APP_URL}/${DISCORD_NOTIFICATIONS_CHANNEL}`,
     {
       json: {
         content: message
@@ -14,7 +18,7 @@ export const publishMessage = (message: string) => {
 export const testPublishMessage = (message: string) => {
   const formattedMessage = `DEV ${message}`;
   return request.post(
-    "https://discordapp.com/api/webhooks/940746404366794782/6cc1whDk-RMckJEktfTSoOSAuXtCgHjOf26yY-zlYc3SPNjBFlISLatkp5iCtxNbvE1c",
+    `${DISCORD_APP_URL}/${DISCORD_TEST_NOTIFICATIONS_CHANNEL}`,
     {
       json: {
         content: formattedMessage
