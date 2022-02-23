e23cab21f42a42d6dc9af722d48cf0ce1edaa65c
faraz ahmad <farazamiruddinn@gmail.com>
2022-02-23 12:45:52 -0500

    add retro stage stepper to board

---
+++ web/src/components/RetroBoardStageStepper.tsx
@@ -0,0 +1,138 @@
+import * as React from "react";
+import { CheckIcon } from "@heroicons/react/solid";
+import { useRetroState } from "../hooks/use-retro-state";
+
+type Step = {
+  id: string;
+  name: "Brainstorm" | "Vote";
+  href: string;
+  status: "current" | "upcoming" | "complete";
+};
+
+// Eventually we will have more stages, but for now only supporting
+// Brainstorm and Vote.
+const steps: Step[] = [
+  { id: "01", name: "Brainstorm", href: "#", status: "current" },
+  // { id: "02", name: "Group", href: "#", status: "upcoming" },
+  { id: "02", name: "Vote", href: "#", status: "upcoming" }
+  // { id: "04", name: "Discuss", href: "#", status: "upcoming" },
+  // { id: "05", name: "Review", href: "#", status: "upcoming" }
+];
+
+function reducer(
+  state: Step[],
+  action: { type: string; payload: "Brainstorm" | "Vote" }
+): Step[] {
+  switch (action.type) {
+    case "change": {
+      const index = steps.findIndex((step) => step.name === action.payload);
+      return state.map((step, stepIdx) => {
+        if (stepIdx === index) {
+          return { ...step, status: "current" };
+        }
+        if (stepIdx < index) {
+          return { ...step, status: "complete" };
+        }
+        if (stepIdx > index) {
+          return { ...step, status: "upcoming" };
+        }
+        return step;
+      });
+    }
+    default: {
+      return state;
+    }
+  }
+}
+
+export function RetroBoardStageStepper({ retroId }: { retroId: string }) {
+  const { state: retro, handleChangeStage } = useRetroState(retroId);
+  const retroStage = retro?.data?.stage;
+  const [state, dispatch] = React.useReducer(reducer, steps);
+
+  React.useEffect(() => {
+    if (!retroStage) {
+      // retroStage isn't present on older retros, so we default to brainstorm
+      // for backwards compatibility.
+      handleChangeStage("Brainstorm");
+      return;
+    }
+    dispatch({ type: "change", payload: retroStage });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [retroStage]);
+
+  return (
+    <nav aria-label="retro-stage">
+      <ol className="border border-gray divide-y divide-gray md:flex md:divide-y-0">
+        {state.map((step, stepIdx) => (
+          <li
+            key={step.name}
+            className="relative md:flex-1 md:flex"
+            onClick={() => handleChangeStage(step.name)}
+          >
+            {step.status === "complete" ? (
+              <a href={step.href} className="group flex items-center w-full">
+                <span className="px-6 py-4 flex items-center text-sm font-medium">
+                  <span className="flex-shrink-0 w-10 h-10 flex items-center justify-center bg-blue rounded-full group-hover:bg-blue">
+                    <CheckIcon className="w-6 h-6 text-white" aria-hidden="true" />
+                  </span>
+                  <span className="ml-4 text-sm font-medium text-blue hover:underline">
+                    {step.name}
+                  </span>
+                </span>
+              </a>
+            ) : step.status === "current" ? (
+              <a
+                href={step.href}
+                className="px-6 py-4 flex items-center text-sm font-medium"
+                aria-current="step"
+              >
+                <span className="flex-shrink-0 w-10 h-10 flex items-center justify-center border-2 border-blue rounded-full">
+                  <span className="text-blue">{step.id}</span>
+                </span>
+                <span className="ml-4 text-sm font-medium text-blue hover:underline">
+                  {step.name}
+                </span>
+              </a>
+            ) : (
+              <a href={step.href} className="group flex items-center">
+                <span className="px-6 py-4 flex items-center text-sm font-medium ">
+                  <span className="flex-shrink-0 w-10 h-10 flex items-center justify-center border-2 border-gray rounded-full">
+                    <span className="text-gray">{step.id}</span>
+                  </span>
+                  <span className="ml-4 text-sm font-medium text-gray group-hover:text-gray hover:underline">
+                    {step.name}
+                  </span>
+                </span>
+              </a>
+            )}
+
+            {stepIdx !== steps.length - 1 ? (
+              <>
+                {/* Arrow separator for lg screens and up */}
+                <div
+                  className="hidden md:block absolute top-0 right-0 h-full w-5"
+                  aria-hidden="true"
+                >
+                  <svg
+                    className="h-full w-full text-gray"
+                    viewBox="0 0 22 80"
+                    fill="none"
+                    preserveAspectRatio="none"
+                  >
+                    <path
+                      d="M0 -2L20 40L0 82"
+                      vectorEffect="non-scaling-stroke"
+                      stroke="currentcolor"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </div>
+              </>
+            ) : null}
+          </li>
+        ))}
+      </ol>
+    </nav>
+  );
+}
