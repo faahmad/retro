e258c0b122074fd96ba9ba06ec22390c5e6504b7
faraz ahmad <farazamiruddinn@gmail.com>
2022-03-03 11:09:43 -0500

    Owner should be able to remove a workspace user

--- web/src/hooks/use-remove-workspace-user.ts
+++ web/src/hooks/use-remove-workspace-user.ts
@@ -9,33 +9,45 @@ function removeWorkspaceUserTransaction(
   userId: User["id"]
 ) {
   return firebase.firestore().runTransaction(async (transaction) => {
+    // Transactions require us to do all the reads first.
+    // Get the user document.
     const userRef = firebase
       .firestore()
       .collection(FirestoreCollections.USER)
       .doc(userId);
     const userDoc = await userRef.get();
     const user = userDoc.data() as User;
-    await transaction.update(userRef, {
-      workspaces: user.workspaces.filter((workspace) => workspace.id !== workspaceId)
-    });
-
+    // Get the workspace invite document.
     const workspaceInviteRef = firebase
       .firestore()
       .collection(FirestoreCollections.WORKSPACE_INVITE)
       .doc(`${workspaceId}_${user.email}`);
-    await transaction.delete(workspaceInviteRef);
+    const workspaceInviteDoc = await workspaceInviteRef.get();
 
+    // Update the user's document.
+    await transaction.update(userRef, {
+      workspaces: user.workspaces.filter((workspace) => workspace.id !== workspaceId)
+    });
+
+    // Delete the workspace invite document.
+    // We have to check if the invite exists because there is a case where we delete the person
+    // who created the workspace initially. In that scenario, they wouldn't have a workspace invite document.
+    if (workspaceInviteDoc.exists) {
+      await transaction.delete(workspaceInviteRef);
+    }
+
+    // Delete the workspace user document.
     const workspaceUserRef = firebase
       .firestore()
       .collection(FirestoreCollections.WORKSPACE_USER)
       .doc(`${workspaceId}_${userId}`);
     await transaction.delete(workspaceUserRef);
 
+    // Update the workspace's userCount field.
     const workspaceRef = firebase
       .firestore()
       .collection(FirestoreCollections.WORKSPACE)
       .doc(workspaceId);
-
     await transaction.update(workspaceRef, {
       userCount: decrement()
     });
