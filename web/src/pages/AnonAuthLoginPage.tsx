036e13fa0beeb32aba4bf6728abfe26e3da3b1f3
faraz ahmad <farazamiruddinn@gmail.com>
2022-03-04 12:54:59 -0500

    create user document on anon signup page

--- web/src/pages/AnonAuthLoginPage.tsx
+++ web/src/pages/AnonAuthLoginPage.tsx
@@ -1,5 +1,5 @@
-import * as React from 'react';
-import { useParams } from 'react-router-dom';
+import * as React from "react";
+import { useParams } from "react-router-dom";
 import { PageContainer } from "../components/PageContainer";
 import { Button } from "../components/Button";
 import { TermsAndConditionsText } from "./LoginPage";
@@ -7,16 +7,37 @@ import { ErrorMessageBanner } from "../components/ErrorMessageBanner";
 import { FirestoreCollections } from "../constants/firestore-collections";
 import firebase from "../lib/firebase";
 import * as Sentry from "@sentry/react";
-import { isNewUser } from '../services/auth-service';
+import { isNewUser } from "../services/auth-service";
+import { User } from "../types/user";
 
 function useCreateAnonWorkspaceUser() {
-
-  async function handleCreate(input: { workspaceId: string, userId: string, displayName: string}) {
-
+  async function handleCreate(input: {
+    workspaceId: string;
+    userId: string;
+    displayName: string;
+  }) {
     return firebase.firestore().runTransaction(async (transaction) => {
-      const workspaceUserRef = firebase.firestore().collection(FirestoreCollections.WORKSPACE_USER).doc(
-        `${input.workspaceId}_${input.userId}`
-      );
+      // Create a user document.
+      const userRef = firebase
+        .firestore()
+        .collection(FirestoreCollections.USER)
+        .doc(input.userId);
+      const newUser: User = {
+        id: input?.userId,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        displayName: input.displayName,
+        email: null,
+        phoneNumber: null,
+        photoUrl: null,
+        workspaces: []
+      };
+      await transaction.set(userRef, newUser);
+
+      // Create a workspace user document.
+      const workspaceUserRef = firebase
+        .firestore()
+        .collection(FirestoreCollections.WORKSPACE_USER)
+        .doc(`${input.workspaceId}_${input.userId}`);
       await transaction.set(workspaceUserRef, {
         userDisplayName: input.displayName,
         workspaceId: input.workspaceId,
@@ -25,78 +46,84 @@ function useCreateAnonWorkspaceUser() {
         userPhotoURL: null,
         userRole: "member"
       });
-    })
+    });
   }
 
-  return handleCreate
+  return handleCreate;
 }
 
 export function AnonAuthLoginPage() {
-    const { workspaceId } = useParams<{workspaceId: string}>();
-    const createAnonWorkspaceUser = useCreateAnonWorkspaceUser();
-    const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
-    const [isSending, setIsSending] = React.useState(false);
-    const [userName, setUserName] = React.useState("");
+  const { workspaceId } = useParams<{ workspaceId: string }>();
+  const createAnonWorkspaceUser = useCreateAnonWorkspaceUser();
+  const [errorMessage, setErrorMessage] = React.useState<string | null>(null);
+  const [isSending, setIsSending] = React.useState(false);
+  const [userName, setUserName] = React.useState("");
 
-    // need to get the workspace name somehow
-    const handleSubmit = async (e: any) => {
-        try {
-            e.preventDefault()
-            if (!userName.trim()) throw new Error("Invalid user name");
+  // need to get the workspace name somehow
+  const handleSubmit = async (e: any) => {
+    try {
+      e.preventDefault();
+      if (!userName.trim()) throw new Error("Invalid user name");
 
-            const userCredential = await firebase.auth().signInAnonymously();
+      const userCredential = await firebase.auth().signInAnonymously();
 
-            if (isNewUser(userCredential) && userCredential.user) {
-              await createAnonWorkspaceUser({
-                workspaceId,
-                displayName: userName,
-                userId: userCredential.user?.uid
-              })
-            }
-        } catch (error) {
-            setIsSending(false);
-            setErrorMessage(error.message);
-            Sentry.captureException(error);
-            return;
-        }
+      if (isNewUser(userCredential) && userCredential.user) {
+        await createAnonWorkspaceUser({
+          workspaceId,
+          displayName: userName,
+          userId: userCredential.user?.uid
+        });
+      }
+    } catch (error) {
+      setIsSending(false);
+      setErrorMessage(error.message);
+      Sentry.captureException(error);
+      return;
     }
+  };
 
-    return (
-        <PageContainer>
-            {errorMessage && <ErrorMessageBanner message={errorMessage} />}
-            <AnonLoginForm
-                isSending={isSending}
-                onSubmit={handleSubmit}
-                userName={userName}
-                setUserName={setUserName}
-            />
-        </PageContainer>
-    )
+  return (
+    <PageContainer>
+      {errorMessage && <ErrorMessageBanner message={errorMessage} />}
+      <AnonLoginForm
+        isSending={isSending}
+        onSubmit={handleSubmit}
+        userName={userName}
+        setUserName={setUserName}
+      />
+    </PageContainer>
+  );
 }
 
-function AnonLoginForm({ isSending, onSubmit, userName, setUserName, title = "Sign in Anonymously" }: any) {
-    return (
-      <div className="flex flex-col items-center">
-        <h1 className="text-blue text-3xl mb-6 text-center">{title}</h1>
-        <form className="flex flex-col mb-12" onSubmit={onSubmit}>
-          <div className="flex flex-col mb-4">
-            <label className="text-blue text-sm" htmlFor="userName">
-              Username
-            </label>
-            <input
-              type="text"
-              name="userName"
-              placeholder="Please enter a username"
-              className="text-blue border border-red my-1 h-12 sm:h-8 md:h-8 lg:h-8 w-full max-w-md outline-none px-1"
-              value={userName}
-              onChange={(e) => setUserName(e.target.value)}
-            />
-          </div>
-          <Button disabled={isSending} className="text-red" type="submit">
-            {isSending ? "Sending..." : "Sign in"}
-          </Button>
-        </form>
-        <TermsAndConditionsText />
-      </div>
-    );
-  }
+function AnonLoginForm({
+  isSending,
+  onSubmit,
+  userName,
+  setUserName,
+  title = "Sign in Anonymously"
+}: any) {
+  return (
+    <div className="flex flex-col items-center">
+      <h1 className="text-blue text-3xl mb-6 text-center">{title}</h1>
+      <form className="flex flex-col mb-12" onSubmit={onSubmit}>
+        <div className="flex flex-col mb-4">
+          <label className="text-blue text-sm" htmlFor="userName">
+            Username
+          </label>
+          <input
+            type="text"
+            name="userName"
+            placeholder="Please enter a username"
+            className="text-blue border border-red my-1 h-12 sm:h-8 md:h-8 lg:h-8 w-full max-w-md outline-none px-1"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+        </div>
+        <Button disabled={isSending} className="text-red" type="submit">
+          {isSending ? "Sending..." : "Sign in"}
+        </Button>
+      </form>
+      <TermsAndConditionsText />
+    </div>
+  );
+}
