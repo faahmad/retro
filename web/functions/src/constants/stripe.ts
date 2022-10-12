c8da288b3223339df4427acbd94288261a9af10d
faraz ahmad <farazamiruddinn@gmail.com>
2022-10-11 20:16:48 -0400

    fix: remove yearly pricing

--- web/functions/src/constants/stripe.ts
+++ web/functions/src/constants/stripe.ts
@@ -1,12 +1,10 @@
 const isDev = process.env.FUNCTIONS_EMULATOR === "true";
 
 enum DevStripeSubscriptionPlans {
-  PRO_MONTHLY = "plan_HFppfQJn3jDBFj",
-  PRO_YEARLY = "price_1Iat06KZLl59Zw3Xr6NBJHvQ"
+  PRO_MONTHLY = "price_1LmfMDKZLl59Zw3XcQsUWhQU"
 }
 enum ProdStripeSubscriptionPlans {
-  PRO_MONTHLY = "price_1LhucwKZLl59Zw3XntkR7YDJ",
-  PRO_YEARLY = "price_1LhudiKZLl59Zw3XvltpqHMF"
+  PRO_MONTHLY = "price_1LhucwKZLl59Zw3XntkR7YDJ"
 }
 export const StripeSubscriptionPlans = isDev
   ? DevStripeSubscriptionPlans
